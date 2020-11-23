c1f621d44b7644866f6720590a7170a5b7eabfb3
Romina Trazzi <romina.trazzi@posta.it>
2020-11-24 00:58:58 +0100

    Commit grezzo con includes e senza validazioni

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,15 @@
-/* js - campominato
+
+/*==============================================
+=            CODICE JS CAMPOMINATO            =
+===============================================*/
+
+/* 
+
 Descrizione
 Il computer deve generare 16 numeri casuali tra 1 e 100.
-I numeri non possono essere duplicati
+* I numeri non possono essere duplicati
 In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-L’utente non può inserire più volte lo stesso numero.
+* L’utente non può inserire più volte lo stesso numero.
 
 Se il numero è presente nella lista dei numeri generati, la partita termina, 
 altrimenti si continua chiedendo all’utente un altro numero.
@@ -18,7 +24,9 @@ BONUS: (da fare solo se funziona tutto il resto)
 all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
 con difficoltà 0 => tra 1 e 100
 con difficoltà 1 => tra 1 e 80
-con difficoltà 2 => tra 1 e 50  */
+con difficoltà 2 => tra 1 e 50  
+
+*/
 
 // Generiamo numeri casuali tra 1 e 100 tramite una funzione
 function getRandomInt(min, max) {
@@ -31,7 +39,7 @@ function getRandomInt(min, max) {
 var nRandom = [];
 
 for (var i = 0; i < 16; i++) { 
-    nRandom.push(getRandomInt(1, 101)); // Salviamo i numeri casuali come elementi di un array   
+    nRandom.push(getRandomInt(1, 101)); // Salviamo i numeri casuali come elementi dell'array   
 }
 
 console.log(nRandom);
@@ -39,18 +47,24 @@ console.log(nRandom);
 // Chiediamo all'utente di inserire 84 numeri compresi tra 1 e 100 
 // Confrontiamo ciascun numero con gli elementi dell'array tramite un valore flag
 
-var numeroPresente = false;
-
 for (var x = 0; x < 5; x++) {
     var userChoice = Number(prompt("Inserisci un numero compreso tra 0 e 100. Non puoi inserire più volte lo stesso numero"));
-     
-    for (var n = 0; n < 5; n++) {
-        var numberRandom = nRandom[i]; // Salviamo gli elementi dell'array uno per volta in una variabile
-        if (numberRandom === userChoice) {
-            numeroPresente = true;
-            var msg = alert("Hai perso");
+    var numberRandom = nRandom[i]; // Salviamo gli elementi dell'array uno per volta in una variabile
+
+    /* Creiamo una variabile per verificare se la mail inserita fa parte di quelle dell'array.
+    La risposta può essere true o false */
+    var boolean = nRandom.includes(userChoice);  
+        if (boolean == true) {
+            alert("Hai perso!");
+        } else {
+            alert("Continuiamo a giocare!");
         }
     }
 
-} 
+
+// Mostriamo all'utente la lista dei numeri generati dal computer
+var listNumber = document.getElementById("lista_numeri");
+listNumber.innerHTML = nRandom + ", ";  
+
+
     
